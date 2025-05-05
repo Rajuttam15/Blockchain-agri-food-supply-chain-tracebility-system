0e1892d81064e865a21e108c48729590aea82381
Rajuttam15 <rajuttam2005@gmail.com>
2025-05-05 14:09:21 +0530

    commit this message

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/8277224bfdfc440988904e7a55ccfad0",
+      chainId: 11155111,
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"],
+    }
+  }
 };
